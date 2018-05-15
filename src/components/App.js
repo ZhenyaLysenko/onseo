3fd5b0805f616982fb3e258cc16b0a02f0adf34a
Bilichenko Illia <35878967+Bilkoo@users.noreply.github.com>
2018-05-15 14:26:06 +0200

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
+import WhatWeDo from "./WhatWeDo";
+import style from './App.css';
+
+class App extends Component {
+  render() {
+    return (<BrowserRouter>
+      <div>
+        <header>
+          {/*
+          тут буде меню, лого і пошук. Оскільни на кожній сторінці вони не змінні.
+          хотя на Home фон повинен покривати header хзхз
+          */
+          }
+        </header>
+
+        {/*
+        <Route exact path="/" component={Home}/>
+        <Route path="/about" component={About}/>
+        <Route path="/contacts" component={Contacts}/>
+        <Route path="/how-we-do" component={HowWeDo}/>
+        <Route path="/vacancies" component={Vacancies}/>
+        <Route path="/who-trust-us" component={WhoTrustUs}/>
+        */
+        }
+      </div>
+    </BrowserRouter>);
+  }
+}
+
+export default App;
---
+++ src/components/WhatWeDo.js
@@ -0,0 +1,13 @@
+import style from "./WhatWeDo.css";
+import React, {Component} from 'react';
+
+const WhatWeDo = () => (
+  <div>
+    <div className={style.conteiner}>
+      <div className={style.cover}>What we do</div>
+      <div className={style.front}>What we do</div>
+    </div>
+  </div>
+);
+
+export default WhatWeDo;
