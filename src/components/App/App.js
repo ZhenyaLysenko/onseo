d64f40487e266fc4d9db8093b605b05b33fff9e9
Pavlivskiy <pavlivskiyvolodymyr@gmail.com>
2018-05-23 11:21:00 +0300

    modified

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,39 +1,20 @@
 import React, {Component} from 'react';
 import {BrowserRouter, Route, Link} from 'react-router-dom';
+import Popup from 'reactjs-popup';
 import style from './App.css';
+import Header from '../header/Header';
 import WhatWeDo from '../WhatWeDo/WhatWeDo'
-
-const Home = () => (
-  <div>
-    <h3>Home</h3>
-  </div>
-)
-
-const About = () => (
-  <div>
-    <h3>About</h3>
-  </div>
-)
-
+import Home from '../Home/Home';
 
 class App extends Component {
   render() {
     return (<BrowserRouter>
       <div>
         <header>
-          {/*
-          тут буде меню, лого і пошук. Оскільни на кожній сторінці вони не змінні.
-          хотя на Home фон повинен покривати header хзхз
-
-          Щоб добавити лінк можна писати
-          <Link to="/about">About</Link>
-          */
-
-          }
+          <Header></Header>
         </header>
-        <Link to="/about">about</Link>
         <Route exact path="/" component={Home}/>
-        <Route exact path="/about" component={WhatWeDo}/>
+        <Route path="/about" component={WhatWeDo}/>
 
       </div>
     </BrowserRouter>);
