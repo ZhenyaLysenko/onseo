44dabe91011c0bd77d2d0eca4c77a61771debbc1
Pavlivskiy <pavlivskiyvolodymyr@gmail.com>
2018-06-14 17:52:38 +0300

    modified

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,4 @@
 import React, {Component} from 'react';
-import {BrowserRouter, Route, Link} from 'react-router-dom';
-import Popup from 'reactjs-popup';
-import style from './App.css';
 import Header from '../header/Header';
 import WhatWeDo from '../WhatWeDo/WhatWeDo'
 import Home from '../Home/Home';
@@ -9,12 +6,44 @@ import Contacts from '../Contacts/Contacts';
 import About from '../About/About';
 import WhoTrustUs from '../WhoTrustUs/WhoTrustUs';
 import { injectGlobal } from 'styled-components';
+import Scroll from 'react-scroll';
 import myFont from '../../../public/fonts/BebasNeueBold.ttf';
+import myFont1 from '../../../public/fonts/ProximaNovaCondensedSemibold.otf';
+import myFont2 from '../../../public/fonts/ProximaNovaCondensedRegular.otf';
+import myFont3 from '../../../public/fonts/ProximaNovaCondensedSemibold.otf';
+import HowWeDo from '../HowWeDo/HowWeDo';
+import Vacansies from '../Vacansies/Vacansies';
+
+var Element = Scroll.Element;
 
 injectGlobal`
   @font-face {
     font-family: 'BebasNenuBold';
     src: url(${myFont}) format('truetype');
+    font-weight: normal;
+    font-style: normal;
+  }
+`;
+injectGlobal`
+  @font-face {
+    font-family: 'ProximaNovaCondensedSemibold';
+    src: url(${myFont1}) format('truetype');
+    font-weight: normal;
+    font-style: normal;
+  }
+`;
+injectGlobal`
+  @font-face {
+    font-family: 'ProximaNovaCondensedRegular';
+    src: url(${myFont2}) format('truetype');
+    font-weight: normal;
+    font-style: normal;
+  }
+`;
+injectGlobal`
+  @font-face {
+    font-family: 'ProximaNovaCondensedSemibold';
+    src: url(${myFont3}) format('truetype');
     font-weight: normal;
     font-style: normal;
   }
@@ -27,11 +56,34 @@ class App extends Component {
         <header>
           <Header></Header>
         </header>
-        <Route exact path="/" component={Home}/>
+        {/* <Route exact path="/" component={Home}/>
         <Route path="/what-we-do" component={WhatWeDo}/>
         <Route path="/contacts" component={Contacts}/>
         <Route path="/about" component={About}/>
         <Route path="/who-trust-us" component={WhoTrustUs}/>
+        <Route path="/how-we-do" component={HowWeDo}/>
+        <Route path="/vacansies" component={Vacansies}/> */}
+        <Element name="Home">
+          <Home></Home>
+        </Element>
+        <Element name="About">
+          <About></About>
+        </Element>
+        <Element name="WhoTrustUs">
+          <WhoTrustUs></WhoTrustUs>
+        </Element>
+        <Element name="WhatWeDo">
+          <WhatWeDo></WhatWeDo>
+        </Element>
+        <Element name="HowWeDo">
+          <HowWeDo></HowWeDo>
+        </Element>
+        <Element name="Vacansies">
+          <Vacansies></Vacansies>
+        </Element>
+        <Element name="Contacts">
+          <Contacts></Contacts>
+        </Element>
       </div>
     );
   }
